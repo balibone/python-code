25af8309eef0aceb5d08472ace92f5a48df18612
Derian <derianpt21@gmail.com>
2019-09-20 22:04:57 +0800

    python3 solution for middle character using integer division

--- codewars/7kyu/middle_character.py
+++ codewars/7kyu/middle_character.py
@@ -1,7 +1,10 @@
 def get_middle(s):
     word_length = len(s)
-    mid_index = (word_length-1) / 2
+    mid_index = (word_length-1) // 2
     if word_length % 2 == 0:
         return s[mid_index:mid_index+2]
     else:
         return s[mid_index]
+
+
+print(get_middle("hell"))
